c35e7e73ef3c3e3f061ea3adbfa6e8c5b90d451b
richard <mr.juggernog@hotmail.com>
2020-12-17 12:27:35 -0600

    Añadido el contenido por default para el portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Ricardo De Leon",
+    title: `Ricardo De Leon`,
+    description: `Este es mi sitio web, donde podras ver mi portfolio de proyectos y saber mas sobre mis habilidades y conocimientos.`,
+    author: `@richidoes`,
   },
   plugins: [
     "gatsby-plugin-styled-components",
@@ -10,6 +12,21 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
       },
     },
   ],
